60058aab4b81c02afd551ecd11225aeb84805068
boborazokov <boborazokovfozil01@gmail.com>
2024-07-29 18:54:03 +0500

    change files

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById("input");
+let res = document.getElementById("res");
+
+function re12() {
+  var re1 = input.value;
+  res.innerHTML += `<img src="${re1}" alt="">`;
+}
